b3519d04b9054ecff58a74466294ae11b8d10886
labinochka <labensckayam@yandex.ru>
2022-09-04 20:05:01 +0300

    Чето ещё сделала

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ class Main {
     public static void main(String[] args) {
         System.out.println("124");
         System.out.println("jfj");
+        System.out.println("wow!!!!!!!!!!");
     }
 }
